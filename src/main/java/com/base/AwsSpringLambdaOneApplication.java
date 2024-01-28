480ec8d12a767c19a3926282e7e34661d35f2056
voroninv <v84venomous@gmail.com>
2024-01-28 18:42:08 +0200

    Initial commit.

---
+++ src/main/java/com/base/AwsSpringLambdaOneApplication.java
@@ -0,0 +1,43 @@
+package com.base;
+
+import com.base.domain.Order;
+import com.base.repo.OrderDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class AwsSpringLambdaOneApplication {
+
+    @Autowired
+    private OrderDao orderDao;
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsSpringLambdaOneApplication.class, args);
+
+    }
+
+    @Bean
+    public Supplier<List<Order>> orders() {
+        return () -> orderDao.buildOrders();
+    }
+
+    @Bean
+    public Function<String, List<Order>> findOrderByName() {
+        return (input) -> orderDao.buildOrders().stream()
+                .filter(order -> order.getName().equals(input))
+                .collect(Collectors.toList());
+    }
+
+    @Bean
+    public Function<String, String> reverse() {
+        return s -> new StringBuilder(s).reverse().toString();
+    }
+
+}
---
+++ src/main/java/com/base/domain/Order.java
@@ -0,0 +1,17 @@
+package com.base.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    private int id;
+    private String name;
+    private double price;
+    private int quantity;
+}
